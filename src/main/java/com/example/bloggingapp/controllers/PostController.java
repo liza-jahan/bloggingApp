7154ce3ffb6c0a3c825113d19229bb8634e63446
Eakteker jahan Liza <74711092+liza-jahan@users.noreply.github.com>
2024-08-07 00:57:12 +0600

    #Task-3 Post

--- src/main/java/com/example/bloggingapp/controllers/PostController.java
+++ src/main/java/com/example/bloggingapp/controllers/PostController.java
@@ -1,6 +1,7 @@
 package com.example.bloggingapp.controllers;
 
 import com.example.bloggingapp.entites.Post;
+import com.example.bloggingapp.payloads.ApiResponse;
 import com.example.bloggingapp.payloads.PostDto;
 import com.example.bloggingapp.services.PostService;
 import lombok.AllArgsConstructor;
@@ -14,39 +15,56 @@ import java.util.List;
 @RequestMapping("/api/")
 @AllArgsConstructor
 public class PostController {
-    private  final PostService postService;
+    private final PostService postService;
+
     @PostMapping("/user/{userId}/category/{categoryId}/posts")
-    public ResponseEntity<PostDto> createPost(
-            @RequestBody  PostDto postDto, @PathVariable Integer userId,
-            @PathVariable Integer categoryId){
-        PostDto createPost=this.postService.createPost(postDto,userId,categoryId);
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto, @PathVariable Integer userId, @PathVariable Integer categoryId) {
+        PostDto createPost = this.postService.createPost(postDto, userId, categoryId);
         return new ResponseEntity<PostDto>(createPost, HttpStatus.CREATED);
     }
+
     //get by user
-  @GetMapping("/user/{userId}/posts")
-    public ResponseEntity<List<PostDto>> getPostByUser(@PathVariable Integer userId){
-       List<PostDto> postDtos= this.postService.getPostsByUser(userId);
-       return new ResponseEntity<List<PostDto>> (postDtos,HttpStatus.OK);
+    @GetMapping("/user/{userId}/posts")
+    public ResponseEntity<List<PostDto>> getPostByUser(@PathVariable Integer userId) {
+        List<PostDto> postDtos = this.postService.getPostsByUser(userId);
+        return new ResponseEntity<List<PostDto>>(postDtos, HttpStatus.OK);
 
     }
+
     //get by category
     @GetMapping("/category/{categoryId}/posts")
-    public ResponseEntity<List<PostDto>> getPostByCategory(@PathVariable Integer categoryId){
-        List<PostDto> postDtos= this.postService.getPostsByCategory(categoryId);
-        return new ResponseEntity<List<PostDto>> (postDtos,HttpStatus.OK);
+    public ResponseEntity<List<PostDto>> getPostByCategory(@PathVariable Integer categoryId) {
+        List<PostDto> postDtos = this.postService.getPostsByCategory(categoryId);
+        return new ResponseEntity<List<PostDto>>(postDtos, HttpStatus.OK);
 
     }
 
     //get All posts
     @GetMapping("/posts")
-    public ResponseEntity<List<PostDto>> getAllPosts(){
-       List<PostDto>postDto=  this.postService.getAllPost();
-       return new ResponseEntity<List<PostDto>>(postDto,HttpStatus.OK);
+    public ResponseEntity<List<PostDto>> getAllPosts() {
+        List<PostDto> postDto = this.postService.getAllPost();
+        return new ResponseEntity<List<PostDto>>(postDto, HttpStatus.OK);
     }
+
     //get post details by id
     @GetMapping("/posts/{postId}")
-    public ResponseEntity<PostDto> getPostById(@PathVariable Integer postId){
-        PostDto postDto=  this.postService.getPostById(postId);
-        return new ResponseEntity<PostDto>(postDto,HttpStatus.OK);
+    public ResponseEntity<PostDto> getPostById(@PathVariable Integer postId) {
+        PostDto postDto = this.postService.getPostById(postId);
+        return new ResponseEntity<PostDto>(postDto, HttpStatus.OK);
+    }
+
+    //delete post
+    @DeleteMapping("/deletePost/{postId}")
+    public ApiResponse deleteByPost(@PathVariable Integer postId) {
+        this.postService.deletePost(postId);
+        return new ApiResponse("Post is deleted", true);
+
+    }
+
+    //update post
+    @GetMapping("/updatePost/{postId}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable Integer postId, @RequestBody PostDto postDto) {
+        PostDto updatePost = this.postService.upadtePost(postDto, postId);
+        return new ResponseEntity<PostDto>(updatePost, HttpStatus.OK);
     }
 }
