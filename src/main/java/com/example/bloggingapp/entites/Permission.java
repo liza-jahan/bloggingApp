49e26252914c044bf5bfbdc2c1d315523c1d95b8
Eakteker jahan Liza <74711092+liza-jahan@users.noreply.github.com>
2024-08-19 00:32:28 +0600

    #Task-9 Jwt JwtTokenHelper

---
+++ src/main/java/com/example/bloggingapp/entites/Permission.java
@@ -0,0 +1,30 @@
+package com.example.bloggingapp.entites;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "permissions")
+public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Permission name is required")
+    private String name;
+
+    private String description;
+
+    private boolean requiresVerification;
+}
---
+++ src/main/java/com/example/bloggingapp/utills/StringUtils.java
@@ -0,0 +1,6 @@
+package com.example.bloggingapp.utills;
+
+public class StringUtils {
+    public static final String PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$";
+
+}
